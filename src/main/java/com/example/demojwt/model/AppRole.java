f63b9e38e59d2a569e9e6384351692dd925beb61
minhtuan <tuannm1610@gmail.com>
2022-07-10 00:33:52 +0700

    create class AppUser

--- src/main/java/com/example/demojwt/model/AppRole.java
+++ src/main/java/com/example/demojwt/model/AppRole.java
@@ -1,15 +1,14 @@
 package com.example.demojwt.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "role")
 public class AppRole {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
 
     public AppRole() {
---
+++ src/main/java/com/example/demojwt/model/DTO/UserPrinciple.java
@@ -0,0 +1,7 @@
+package com.example.demojwt.model.DTO;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserPrinciple implements UserDetails {
+
+}
