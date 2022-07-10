01215dc7fa50537a4f0f35f775d27e9ae69ef35a
minhtuan <tuannm1610@gmail.com>
2022-07-10 17:24:05 +0700

    JwtService

--- src/main/java/com/example/demojwt/config/custom/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demojwt/config/custom/JwtAuthenticationFilter.java
@@ -1,4 +1,31 @@
 package com.example.demojwt.config.custom;
 
-public class JwtAuthenticationFilter {
+import com.example.demojwt.service.appuser.IAppUserSevice;
+import com.example.demojwt.service.jwt.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private IAppUserSevice userSevice;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+
+        } catch () {
+
+        }
+    }
 }
