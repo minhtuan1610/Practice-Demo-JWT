618bb2c33e741b0528f14202e6e8b6cca5d43394
minhtuan <tuannm1610@gmail.com>
2022-07-10 22:52:41 +0700

    JwtResponse

---
+++ src/main/java/com/example/demojwt/model/DTO/JwtResponse.java
@@ -0,0 +1,66 @@
+package com.example.demojwt.model.DTO;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public class JwtResponse {
+    private Long id;
+    private String token;
+    private String type = "Bearer";
+    private String username;
+    private String name;
+    private Collection<? extends GrantedAuthority> roles;
+
+    public JwtResponse(Long id, String accessToken, String username, String name, Collection<? extends GrantedAuthority> roles) {
+        this.id = id;
+        this.token = accessToken;
+        this.username = username;
+        this.name = name;
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAccessToken() {
+        return token;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.token = accessToken;
+    }
+
+    public String getTokenType() {
+        return type;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.type = tokenType;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Collection<? extends GrantedAuthority> getRoles() {
+        return roles;
+    }
+}
