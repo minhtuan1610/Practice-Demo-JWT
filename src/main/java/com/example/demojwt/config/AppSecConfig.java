affc67b10b8e706614a5a35c10ad9f4fc642ad05
minhtuan <tuannm1610@gmail.com>
2022-07-10 23:16:58 +0700

    cau hinh AppSecConfig

--- src/main/java/com/example/demojwt/config/AppSecConfig.java
+++ src/main/java/com/example/demojwt/config/AppSecConfig.java
@@ -1,15 +1,66 @@
 package com.example.demojwt.config;
 
+import com.example.demojwt.config.custom.CustomAccessDeniedHandler;
+import com.example.demojwt.config.custom.JwtAuthenticationFilter;
+import com.example.demojwt.config.custom.RestAuthenticationEntryPoint;
 import com.example.demojwt.service.appuser.IAppUserSevice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class AppSecConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private IAppUserSevice appUserSevice;
 
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
 
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Bean
+    public RestAuthenticationEntryPoint restAuthenticationEntryPoint() {
+        return new RestAuthenticationEntryPoint();
+    }
+
+    @Bean
+    public CustomAccessDeniedHandler customAccessDeniedHandler() {
+        return new CustomAccessDeniedHandler();
+    }
+
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(appUserSevice).passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/**");
+        http.httpBasic().authenticationEntryPoint(restAuthenticationEntryPoint());
+        http.authorizeRequests()
+                .antMatchers("/", "/login").permitAll()
+                .and().authorizeRequests().antMatchers("/admin**").hasAnyRole("ADMIN")
+                .and().authorizeRequests().antMatchers("/user**").hasAnyRole("USER")
+                .and()
+                .csrf().disable();
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling().accessDeniedHandler(customAccessDeniedHandler());
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.cors();
+    }
 }
